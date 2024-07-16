e356151616c806427a32fe468f893ca0b85a0914
Zaman-Meer <m.zaman.meer@gmail.com>
2024-07-17 01:39:56 +0500

    fix cron job

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
-import { startCronJob } from "@/scripts/cronJob";
-
-startCronJob();
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/scripts/cronJob.ts
+++
@@ -1,6 +0,0 @@
-import cron from "node-cron";
-import { updateBlocks } from "@/utils/updateBlocks";
-
-export const startCronJob = () => {
-  cron.schedule("*/30 * * * * *", updateBlocks);
-};
